645bbcf0cffab2e81051df74a49a913a444675bb
Tannu Rajput <radhikac5522@gmail.com>
2026-05-20 10:50:45 +0530

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,9 @@
+print("Calculator")
+
+a = int(input("First Number: "))
+b = int(input("Second Number: "))
+
+print("Add:", a + b)
+print("Subtract:", a - b)
+print("Multiply:", a * b)
+print("Divide:", a / b)
